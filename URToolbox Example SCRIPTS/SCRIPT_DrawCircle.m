4ecefd54be35d9b0404ff7ff0a6adcab60fca739
kutzer <kutzer@usna.edu>
2018-03-14 09:43:47 -0400

    Made SCRIPT_DrawCircle platform agnostic.

--- URToolbox Example SCRIPTS/SCRIPT_DrawCircle.m
+++ URToolbox Example SCRIPTS/SCRIPT_DrawCircle.m
@@ -7,7 +7,7 @@
 %
 %   M. Kutzer, 14Mar2018, USNA
 
-clearvars -EXCEPT ur5 hw5
+clearvars -EXCEPT simObj hwObj
 close all
 clc
 
@@ -17,22 +17,22 @@ clc
 useHardware = false;
 
 %% Initialize simulation
-% -> This code is written assuming use of the UR5 system. Minor adjustments
-% can be made to use it with the UR3 or UR10
+% -> This code has been tested on the UR5 and UR10 systems. Minor 
+% adjustments may be required to use it with the UR3.
 
-if ~exist('ur5')
+if ~exist('simObj')
     % Create object
-    ur5 = URsim;
+    simObj = URsim;
     % Initialize simulation
-    ur5.Initialize('UR5');
+    simObj.Initialize;
     % Set a tool frame offset (e.g. for Robotiq end-effector not shown in
     % visualization)
-    ur5.FrameT = Tz(160);
+    simObj.FrameT = Tz(160);
     
     % Hide frames
     frames = '0123456E';
     for i = 1:numel(frames)
-        hideTriad(ur5.(sprintf('hFrame%s',frames(i))));
+        hideTriad(simObj.(sprintf('hFrame%s',frames(i))));
     end
 end
 
@@ -40,14 +40,15 @@ end
 % -> The message to the user *assumes* that you have:
 %       (1) Properly installed URToolboxScript on the UR controller. See
 %       the link below for instructions:
-%           https://www.usna.edu/Users/weapsys/kutzer/_Code-Development/UR_Toolbox.php
+%       
+%       https://www.usna.edu/Users/weapsys/kutzer/_Code-Development/UR_Toolbox.php
 %
-%       (2) Set the network card connecting the PC and UR controller to a
-%       static IP of 10.1.1.5
+%       (2) Configure the network card connecting the PC to the UR 
+%       controller to a static IP of 10.1.1.5
 %
 %       (3) Set the UR controller IP to 10.1.1.2
 
-if ~exist('hw5') && useHardware
+if ~exist('hwObj') && useHardware
     instruct = sprintf([...
         '\tPython module imported.\n',...
         '\tEnter server IP address: 10.1.1.5\n',...
@@ -62,7 +63,7 @@ if ~exist('hw5') && useHardware
     fprintf('PLEASE USE THE FOLLOWING RESPONSES:\n\n');
     fprintf(2,'%s\n\n',instruct)
     
-    hw5 = UR;
+    hwObj = UR;
 end
 
 %% Create path
@@ -85,22 +86,22 @@ X(4,:) = 1;             % Append 1 (homogeneous coordinate)
 X = Tz(500)*Rx(pi/2)*Tz(500)*X;
 
 % Plot waypoints
-plt_Waypoints = plot3(ur5.Axes,X(1,:),X(2,:),X(3,:),'.m');
+plt_Waypoints = plot3(simObj.Axes,X(1,:),X(2,:),X(3,:),'.m');
 
 %% Animate simulation and move the robot to test
 % Home simulation
-ur5.Home;
+simObj.Home;
 % Move robot to match simulation
 if useHardware
     % Get joint position from the simulation
-    q = ur5.Joints;
+    q = simObj.Joints;
     % Send waypoint to the robot
     % -> Message syntax sends 6 joint positions and 6 joint velocities to
     % define a desired waypoint for the URToolboxScript controller. We are
     % assuming a desired velocity of zero for all joints at each waypoint.
-    msg(hw5,sprintf('(%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f)',q,zeros(6,1)));
+    msg(hwObj,sprintf('(%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f)',q,zeros(6,1)));
     % Wait for the robot to finish executing the move
-    urWaitForMove(hw5);
+    urWaitForMove(hwObj);
 end
 % Allow plot to update
 drawnow
@@ -111,19 +112,19 @@ for i = 1:numel(s)
     H_cur = Tx(X(1,i))*Ty(X(2,i))*Tz(X(3,i))*Rx(pi/2);
     
     % Set simulation toolpose to waypoint pose
-    ur5.ToolPose = H_cur;
+    simObj.ToolPose = H_cur;
     
     % Move robot to match simulation
-    q = ur5.Joints;
+    q = simObj.Joints;
     if useHardware
         % Get joint position from the simulation
-        q = ur5.Joints;
+        q = simObj.Joints;
         % Send waypoint to the robot
-        msg(hw5,sprintf('(%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f)',q,zeros(6,1)));
+        msg(hwObj,sprintf('(%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f)',q,zeros(6,1)));
         % Wait for move only on the first waypoint
         if i == 1
             % Wait for the robot to finish executing the move
-            urWaitForMove(hw5);
+            urWaitForMove(hwObj);
         end
     end
     
